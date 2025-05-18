8a2f313ac163f566fc4c78984d7ff60afad48bde
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-05-18 21:26:00 +0600

    Meal DB first commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <section className="home w-full">
+            <div className="home-inner container mx-auto px-6 py-14">
+                <h1>Meal DB home page.</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
